c3c83c9e6b255ce751f3bb2c9770a42e3fff77f6
Vincent Hong <vincent.hong@ufl.edu>
2026-03-21 22:10:59 -0400

    Set up welcome page and added login/ sign up button in the header of welcoem page. Set up Login and Sign up pages, along with checks on passwords when signing up. Clicking on logo and website name goes back to welcome page.

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,16 @@
+import './Login.css';
+
 function Login() {
 	return (
-		<div className="login ">
-			<h1>Login</h1>
+		<div className="login-page">
+			<div className="login-box">
+				<h1>Login</h1>
+				<form className='login-form'>
+					<input type="email"		placeholder="Email"		className="input-field"/> <br />
+					<input type="password"	placeholder="Password"	className="input-field"/> <br />
+					<button type="submit" className="submit-btn">Login</button>
+				</form>
+			</div>
 		</div>
 	);
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,22 @@ export default function Header() {
 	const location = useLocation();
 
 	const isWelcomePage = location.pathname === "/";
+	const isLoginPage = location.pathname === "/login";
+	const isSignUpPage = location.pathname === "/signup";
 
 	return (
 		<header className="header-container">
-			<div className="logo-wrapper">
-				<img src="/eventify-logo-clear.png" alt="Eventify Logo" className="logo-img" />
-				<h3 className="brand-name">Eventify</h3>
-			</div>
+			{(isLoginPage || isSignUpPage) ? (
+				<Link to="/" className="logo-wrapper">
+					<img src="/eventify-logo-clear.png" alt="Eventify Logo" className="logo-img" />
+					<h3 className="brand-name">Eventify</h3>
+				</Link>
+			) : (
+				<div to="/" className="logo-wrapper">
+					<img src="/eventify-logo-clear.png" alt="Eventify Logo" className="logo-img" />
+					<h3 className="brand-name">Eventify</h3>
+				</div>
+			)}
 
 			<nav>
 				{isWelcomePage && (
