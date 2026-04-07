2c7e1d43a06922d2ced92be5a782a0136485a086
logerator <logan.canady@ifoundry.com>
2026-04-07 13:36:37 -0400

    added gradient background, welcome page new elements, events page looks better, still need to implement functionality for remove button in my events

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,17 +10,10 @@ export default function Header() {
 
 	return (
 		<header className="header-container">
-			{isLoginOrSignUpPage ? (
-				<Link to="/" className="logo-wrapper">
-					<img src="/eventify-logo-clear.png" alt="Eventify Logo" className="logo-img" />
-					<h3 className="brand-name">Eventify</h3>
-				</Link>
-			) : (
-				<div className="logo-wrapper">
-					<img src="/eventify-logo-clear.png" alt="Eventify Logo" className="logo-img" />
-					<h3 className="brand-name">Eventify</h3>
-				</div>
-			)}
+			<Link to="/" className="logo-wrapper">
+				<img src="/eventify-logo-clear.png" alt="Eventify Logo" className="logo-img" />
+				<h3 className="brand-name">Eventify</h3>
+			</Link>
 
 			<nav>
 				{(isWelcomePage || isEventsPage) && (
